246ca1fe96b2d01163fb3827ad2f75361ab58216
Ivan Kyuchukov <55844949+IvanKyuchukov@users.noreply.github.com>
2022-05-08 18:38:45 +0300

    Add files via upload

---
+++ Summator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Summator
+{
+   public class Program
+    {
+        static void Main(string[] args)
+        {
+            long result = Summator.Sum(new long[] { 1, 2, 3, 4, 5, 6 });
+            if (result == 21)
+            {
+                Console.WriteLine("pass");
+            }
+            else 
+            {
+                Console.WriteLine("Fail");
+            }
+
+            int average = Summator.Average(new int[] { 1,11,30 });
+            if (average == 14)
+            {
+                Console.WriteLine("pass");
+            }
+            else
+            {
+                Console.WriteLine("Fail");
+            }
+        }
+    }
+}
